8f75243a0fb062bdfdd59dfe6ab1144c361b38e4
Andrej Dvornic <dvornic00@gmail.com>
2024-07-01 20:23:42 +0200

    Category entity and repository created

---
+++ app/src/Entity/Category.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'categories')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $id_user = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $category_name = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $income_or_expense = null;
+
+    #[ORM\Column]
+    private ?bool $is_custom = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdUser(): ?User
+    {
+        return $this->id_user;
+    }
+
+    public function setIdUser(?User $id_user): static
+    {
+        $this->id_user = $id_user;
+
+        return $this;
+    }
+
+    public function getCategoryName(): ?string
+    {
+        return $this->category_name;
+    }
+
+    public function setCategoryName(string $category_name): static
+    {
+        $this->category_name = $category_name;
+
+        return $this;
+    }
+
+    public function getIncomeOrExpense(): ?string
+    {
+        return $this->income_or_expense;
+    }
+
+    public function setIncomeOrExpense(string $income_or_expense): static
+    {
+        $this->income_or_expense = $income_or_expense;
+
+        return $this;
+    }
+
+    public function isCustom(): ?bool
+    {
+        return $this->is_custom;
+    }
+
+    public function setCustom(bool $is_custom): static
+    {
+        $this->is_custom = $is_custom;
+
+        return $this;
+    }
+}
--- app/src/Entity/User.php
+++ app/src/Entity/User.php
@@ -5,6 +5,8 @@ namespace App\Entity;
 use App\Repository\UserRepository;
 use DateTimeImmutable;
 use DateTimeInterface;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
@@ -61,9 +63,16 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     private string $plainPassword;
 
+    /**
+     * @var Collection<int, Category>
+     */
+    #[ORM\OneToMany(targetEntity: Category::class, mappedBy: 'id_user')]
+    private Collection $categories;
+
     public function __construct()
     {
         $this->created_at = new DateTimeImmutable();
+        $this->categories = new ArrayCollection();
     }
 
 
@@ -252,6 +261,36 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setPlainPassword(string $plainPassword): void
     {
         $this->plainPassword = $plainPassword;
+    }
+
+    /**
+     * @return Collection<int, Category>
+     */
+    public function getCategories(): Collection
+    {
+        return $this->categories;
+    }
+
+    public function addCategory(Category $category): static
+    {
+        if (!$this->categories->contains($category)) {
+            $this->categories->add($category);
+            $category->setIdUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCategory(Category $category): static
+    {
+        if ($this->categories->removeElement($category)) {
+            // set the owning side to null (unless already changed)
+            if ($category->getIdUser() === $this) {
+                $category->setIdUser(null);
+            }
+        }
+
+        return $this;
     }
 
 }
