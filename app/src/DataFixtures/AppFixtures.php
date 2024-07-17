d603d35937ae4e1fa535e88d19f450d00328b250
Andrej Dvornic <dvornic00@gmail.com>
2024-07-17 15:12:13 +0200

    Panel overview is completed

--- app/src/DataFixtures/AppFixtures.php
+++ app/src/DataFixtures/AppFixtures.php
@@ -15,12 +15,12 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         // Create admin manually with specific details
-        UserFactory::new([
-            'email' => 'admin@gmail.com',
-            'plainPassword' => 'Password#1',
-            'fullName' => 'Jane Doe',
-            'roles' => ['ROLE_ADMIN']
-        ])->create();
+//        UserFactory::new([
+//            'email' => 'admin@gmail.com',
+//            'plainPassword' => 'Password#1',
+//            'fullName' => 'Jane Doe',
+//            'roles' => ['ROLE_ADMIN']
+//        ])->create();
 
         // Create user manually with specific details
         UserFactory::new([
