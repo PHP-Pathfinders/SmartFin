9b0c200dd424a2a9ce6c76f5f81f9e2b9a7f55e7
blknbro <duledule17113@gmail.com>
2024-07-24 14:07:25 +0200

    Generators doc done

--- app/src/Controller/PdfXlsController.php
+++ app/src/Controller/PdfXlsController.php
@@ -13,7 +13,32 @@ use OpenApi\Attributes as OA;
 class PdfXlsController extends AbstractController
 {
     #[Route('/pdf/generator', name: 'api_pdf_generator', methods: ['GET'])]
-    #[OA\Tag(name: 'Generators')]
+    #[OA\Get(
+        description: 'Used for generating pdf file of all transactions for logged user',
+        summary: 'PDF generator for user\'s transactions',
+        tags: ['Generators'],
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: 'Successful pdf generation',
+            ),
+            new OA\Response(
+                response: 400,
+                description: 'Daily generation limit reached',
+                content: new OA\JsonContent(ref: '#/components/schemas/GeneratorPDFError')
+            ),
+            new OA\Response(
+                response: 401,
+                description: 'Unauthorized access detected',
+                content: new OA\JsonContent(ref: '#/components/schemas/Unauthorized')
+            ),
+            new OA\Response(
+                response: 404,
+                description: 'Invalid category given',
+                content: new OA\JsonContent(ref: '#/components/schemas/UserNotFound')
+            ),
+        ]
+    )]
     public function generatePDF(
         PdfXlsService $pdfXlsService,
     ): Response
@@ -28,7 +53,32 @@ class PdfXlsController extends AbstractController
     }
 
     #[Route('/xls/generator', name: 'api_xls_generator', methods: ['GET'])]
-    #[OA\Tag(name: 'Generators')]
+    #[OA\Get(
+        description: 'Used for generating xlsx file of all transactions for logged user',
+        summary: 'XLS generator for user\'s transactions',
+        tags: ['Generators'],
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: 'Successful xls generation',
+            ),
+            new OA\Response(
+                response: 400,
+                description: 'Daily generation limit reached',
+                content: new OA\JsonContent(ref: '#/components/schemas/GeneratorXLSError')
+            ),
+            new OA\Response(
+                response: 401,
+                description: 'Unauthorized access detected',
+                content: new OA\JsonContent(ref: '#/components/schemas/Unauthorized')
+            ),
+            new OA\Response(
+                response: 404,
+                description: 'Invalid category given',
+                content: new OA\JsonContent(ref: '#/components/schemas/UserNotFound')
+            ),
+        ]
+    )]
     public function generateXLS(
         PdfXlsService $pdfXlsService
     ) :Response
