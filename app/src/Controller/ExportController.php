cc843ed231ca4500cff42d09cbf41d953a90fb5d
blknbro <duledule17113@gmail.com>
2024-07-24 13:55:09 +0200

    Export and Mailer doc done

--- app/src/Controller/ExportController.php
+++ app/src/Controller/ExportController.php
@@ -19,7 +19,38 @@ class ExportController extends AbstractController
      * Get a list of all exports made by a user
      */
     #[Route('', name: 'api_export_list', methods: ['GET'])]
-    #[OA\Tag(name: 'Exports')]
+    #[OA\Get(
+        description: 'Gives a list of all pdf and xls files that are associated with user',
+        summary: 'Get a list of all exports made by a user',
+        tags: ['Exports'],
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: 'Successful response',
+                content: new OA\JsonContent(ref: '#/components/schemas/Export')
+            ),
+            new OA\Response(
+                response: 404,
+                description: 'No exports found',
+                content: new OA\JsonContent(ref: '#/components/schemas/ExportsNotFound')
+            ),
+            new OA\Response(
+                response: 500,
+                description: 'Wrong user id or user not authenticated',
+                content: new OA\JsonContent(ref: '#/components/schemas/ExportsUserNotFound')
+            ),
+            new OA\Response(
+                response: 401,
+                description: 'Unauthorized access detected',
+                content: new OA\JsonContent(ref: '#/components/schemas/Unauthorized')
+            ),
+            new OA\Response(
+                response: 422,
+                description: 'Invalid input data given',
+                content: new OA\JsonContent(ref: '#/components/schemas/ExportsInputError')
+            ),
+        ]
+    )]
     public function search(
         #[MapQueryString] SearchDto $searchDto,
         ExportService $exportService
@@ -42,7 +73,32 @@ class ExportController extends AbstractController
      * Download exported file
      */
     #[Route('/download/{fileName}', name: 'api_export_download', methods: ['GET'])]
-    #[OA\Tag(name: 'Exports')]
+    #[OA\Get(
+        description: 'Download exported files made by a user, this endpoint might not be fully functional here but you can try it out at http://localhost:8080/api/exports/download/{fileName} with correct fileName from /api/exports',
+        summary: 'Enables downloading of exported files associated with user',
+        tags: ['Exports'],
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: 'Successful response',
+            ),
+            new OA\Response(
+                response: 404,
+                description: 'File not found',
+                content: new OA\JsonContent(ref: '#/components/schemas/ExportsFileNotFound')
+            ),
+            new OA\Response(
+                response: 400,
+                description: 'Bad Request',
+                content: new OA\JsonContent(ref: '#/components/schemas/ExportInvalidRequest')
+            ),
+            new OA\Response(
+                response: 401,
+                description: 'Unauthorized access detected',
+                content: new OA\JsonContent(ref: '#/components/schemas/Unauthorized')
+            ),
+        ]
+    )]
     public function downloadFile(
         string $fileName,
         ExportService $exportService
